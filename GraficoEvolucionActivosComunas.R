d09bbe2155a665e223ce21daa130219b87bf3036
Héctor Soto <hector.soto@fundacionmetrica.org>
2020-09-15 18:54:55 -0300

    Resumen
    Se agregan archivos a la carpeta principal

---
+++ GraficoEvolucionActivosComunas.R
@@ -0,0 +1,24 @@
+library(highcharter)
+library(dplyr)
+
+Chartevolucion <- function(datoscomunas, ymaximo){
+  
+  hc <- hchart(datoscomunas, "areaspline", 
+               hcaes(x = Fecha, y = Casos_Activos), 
+               name = "Casos Activos",
+               color = "#6fb9d7") %>%
+    hc_yAxis(title = list(text = "Casos Activos"),
+             max = ymaximo,
+             crosshair = TRUE)%>%
+    hc_xAxis(title = list(text ="Fechas según publicación Informe Epidemiológico"),
+             crosshair = TRUE) %>%
+    hc_title(text = "Evolución de Casos Activos") %>%
+    hc_subtitle(text = paste0("al ", ultimafecha_Activos)) %>%
+    hc_credits(enabled = TRUE, text = "http://www.redcrecemos.cl/") %>%
+    hc_tooltip(sort = TRUE, table = TRUE) %>% 
+    hc_caption(text = "Fuente: Elaboración propia en base a último Informe Epidemiológico")
+  
+  hc
+}
+
+
