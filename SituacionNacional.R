a151a49377c5627c6e7def63a406d9e32501236d
Héctor Soto <hector.soto@fundacionmetrica.org>
2020-09-23 14:52:18 -0300

    Actualizacion del 23 de septiembre
    Actualización segun ultimo reporte epidemiologico del 23 de septiembre

---
+++ SituacionNacional.R
@@ -0,0 +1,31 @@
+#Situacion Nacional 
+
+Situación Nacional {.hidden}
+=======================================================================
+  
+  Row {data-height=150}
+-----------------------------------------------------------------------
+  ### REPORTE SITUACIÓN ACTUALIZADA COVID 19  al `r Sys.Date()`
+  
+  A continuación, presentamos información que esperamos sean útiles en esta pandemia, los cuales iremos actualizando periódicamente.
+
+Row {data-height=850}
+-----------------------------------------------------------------------
+  ### Informe Perceptiva y Cuadro Regional {data-width=400}
+  PERCEPTIVA
+</br>
+  <img src="Perceptiva_2020-09-16.png" alt="" width="550" height="300" > 
+  </br>
+  </br>
+  CUADRO REGIONAL
+</br>
+  <img src="CuadroRegional.jpeg" alt="" width="550" height="250" > 
+  
+  ### Chile {data-width=600}
+  
+  <iframe 
+src="https://e.infogram.com/79acdebc-18ad-4399-8936-bce3b3a49068?parent_url=https%3A%2F%2Fwww.gob.cl%2Fcoronavirus%2Fcifrasoficiales%2F&src=embed#" 
+width="900" 
+height="700" 
+allow="fullscreen">
+  </iframe>
